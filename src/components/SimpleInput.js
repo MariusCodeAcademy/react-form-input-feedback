80ed50dd28aaab33b5887f049e80cb01eb01d20f
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-16 13:44:02 +0300

    using ref

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
   const nameInputChangeHandler = (event) => {
     console.log('keystroke');
     setEnteredName(event.target.value);
@@ -10,12 +11,19 @@ const SimpleInput = (props) => {
     event.preventDefault();
     console.log('SubmitEvent');
     console.log('ivesta', enteredName);
+    // naudojant ref gauti ivesties lauko reiksme
+    const enteredValue = nameInputRef.current.value;
+    console.log('value using ref', enteredValue)
+    // isvalyti input po submit
+    setEnteredName('')
+    // nameInputRef.current.value = '' // nerekomenduojam updatinti dom rankiniu budu
   }
   return (
     <form onSubmit={formSubmissionHandler} >
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input
+          ref={nameInputRef}
           value={enteredName}
           onChange={nameInputChangeHandler}
           type="text"
