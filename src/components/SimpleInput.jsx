b984f3195d40e37c62364b626ec732f9b6c8e00e
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-16 14:04:04 +0300

    invalid feedback start

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,30 +1,41 @@
 import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
   const nameInputRef = useRef();
+
   const nameInputChangeHandler = (event) => {
-    console.log('keystroke');
+    console.log("keystroke");
     setEnteredName(event.target.value);
   };
   const formSubmissionHandler = (event) => {
-    // sustabdyti forma nuo siuntimo nustatytuoju budu 
+    // sustabdyti forma nuo siuntimo nustatytuoju budu
     event.preventDefault();
 
     // validacija
-    if (enteredName.trim() === '') return; 
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
 
-    console.log('SubmitEvent');
-    console.log('ivesta', enteredName);
+    console.log("SubmitEvent");
+    console.log("ivesta", enteredName);
     // naudojant ref gauti ivesties lauko reiksme
     const enteredValue = nameInputRef.current.value;
-    console.log('value using ref', enteredValue)
+    console.log("value using ref", enteredValue);
     // isvalyti input po submit
-    setEnteredName('')
+    setEnteredName("");
+    setEnteredNameIsValid(true);
     // nameInputRef.current.value = '' // nerekomenduojam updatinti dom rankiniu budu
-  }
+  };
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form onSubmit={formSubmissionHandler} >
-      <div className="form-control">
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -33,6 +44,9 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
