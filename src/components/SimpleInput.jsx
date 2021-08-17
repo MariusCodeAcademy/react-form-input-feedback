8875d78555545d0cff40dca97c87791de38bb5d3
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-17 09:43:31 +0300

    added email field + validation

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
+
+// 6. extra kas pasidaret pasidaryti kad name butu tik raides ir tarpai
+// 7. nepamirsti isvalyti email ivesties lauku ir palietimo busenos kai submitinam
+// 8. Kas viska pasidare, pabandyti sukurti Input komponenta su visa sita logika kuris veikia
+// per props
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  // email
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   const [formIsValid, setFormIsValid] = useState(false);
 
@@ -10,10 +18,16 @@ const SimpleInput = (props) => {
 
   const enteredNameIsValid = enteredName.trim().length >= 3;
   const nameInputHasError = !enteredNameIsValid && enteredNameTouched;
+  // email
+  const enteredEmailIsValid =
+    enteredEmail.includes("@") && enteredEmail.includes(".");
+  const emailInputHasError = !enteredEmailIsValid && enteredEmailTouched;
 
   useEffect(() => {
-    enteredNameIsValid ? setFormIsValid(true) : setFormIsValid(false);
-  }, [enteredNameIsValid]);
+    enteredNameIsValid && enteredEmailIsValid
+      ? setFormIsValid(true)
+      : setFormIsValid(false);
+  }, [enteredNameIsValid, enteredEmailIsValid]);
 
   const nameInputChangeHandler = (event) => {
     console.log("keystroke");
@@ -22,6 +36,15 @@ const SimpleInput = (props) => {
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
   };
+  // email
+  const emailInputChangeHandler = (event) => {
+    console.log("keystroke");
+    setEnteredEmail(event.target.value);
+  };
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
+  };
+
   const formSubmissionHandler = (event) => {
     // sustabdyti forma nuo siuntimo nustatytuoju budu
     event.preventDefault();
@@ -30,17 +53,25 @@ const SimpleInput = (props) => {
     setEnteredNameTouched(true);
 
     // validacija
-    if (!enteredNameIsValid) {
+    if (!formIsValid) {
       return;
     }
 
     // isvalyti input po submit
     setEnteredName("");
     setEnteredNameTouched(false);
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
+
+    console.log(enteredName, enteredEmail);
   };
 
   // computed classes
   const nameInputClasses = nameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+  // email
+  const emailInputClasses = emailInputHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -57,6 +88,19 @@ const SimpleInput = (props) => {
         />
         {nameInputHasError && (
           <p className="error-text">Name must be at least 3 letters</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          value={enteredEmail}
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          type="text"
+          id="email"
+        />
+        {emailInputHasError && (
+          <p className="error-text">Please provide valid email</p>
         )}
       </div>
       <div className="form-actions">
